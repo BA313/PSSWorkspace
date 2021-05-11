d5dd02ecd9b92d2e8472513070f3b6be60e9cb1a
Andy Peng <48707379+sjpmpzx@users.noreply.github.com>
2021-05-10 22:32:20 -0700

    Add JsonReader and two test json files

---
+++ PSS/src/Model/JsonReader.java
@@ -0,0 +1,74 @@
+package Model;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class JsonReader {
+
+    /***
+     * Read a json file and return a recurring task list
+     * @param filePath path of the json file
+     * @return an ArrayList of Task, or an empty ArrayList if exception occurs
+     */
+    public ArrayList<Task> readRecurTaskList(String filePath) {
+        Gson gson = new Gson();
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(filePath));
+            return gson.fromJson(reader, new TypeToken<ArrayList<Recurring>>(){}.getType());
+
+        } catch (IOException e) {
+            System.out.println("Error occurs!");
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    /***
+     * Read a json file and return a transient task list
+     * @param filePath path of the json file
+     * @return an ArrayList of Task, or an empty ArrayList if exception occurs
+     */
+    public ArrayList<Task> readTransientTaskList(String filePath) {
+        Gson gson = new Gson();
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(filePath));
+            return gson.fromJson(reader, new TypeToken<ArrayList<Transient>>(){}.getType());
+
+        } catch (IOException e) {
+            System.out.println("Error occurs!");
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    /***
+     * Read a json file and return a task list with both recurring and transient tasks
+     * @param recurTaskFilePath path of the json file
+     * @param transientTaskFilePath path of the json file
+     * @return an ArrayList of Task, or an empty ArrayList if exception occurs
+     */
+    public ArrayList<Task> readTaskList(String recurTaskFilePath, String transientTaskFilePath) {
+        Gson gson = new Gson();
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(recurTaskFilePath));
+            ArrayList<Task> taskList = gson.fromJson(reader, new TypeToken<ArrayList<Recurring>>(){}.getType());
+            reader = Files.newBufferedReader(Paths.get(transientTaskFilePath));
+            taskList.addAll(gson.fromJson(reader, new TypeToken<ArrayList<Transient>>(){}.getType()));
+            return taskList;
+
+        } catch (IOException e) {
+            System.out.println("Error occurs!");
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+
+
+}
