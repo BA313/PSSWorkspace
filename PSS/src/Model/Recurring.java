241e8824f0ccf045b7b37040a23c8f7895a1afd6
Ben Aldrich <benaldrich2000@gmail.com>
2021-05-11 15:13:34 -0700

    Reformatting
    Moved where main runner is located
    Moved where task class is located
    Started to reformat the child classes for task

--- PSS/src/Model/Recurring.java
+++ PSS/src/Model/Recurring.java
@@ -1,8 +1,15 @@
 package Model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Date;
 
 public class Recurring extends Task {
+	
+	public Recurring(String name, LocalDate startDate, LocalDate endDate, int duration, boolean repeat,
+			LocalTime startTime, LocalTime endTime) {
+		super(name, startDate, endDate, duration, repeat, startTime, endTime);
+	}
 	private int repeatBasis;
 	private String task, category;
 	private Date endDate;
