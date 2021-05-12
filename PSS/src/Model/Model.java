d84ef7aa55cad6cfe7ce225d9b0c0f98f344e465
Ben Aldrich <benaldrich2000@gmail.com>
2021-05-11 17:41:19 -0700

    Format changes

--- PSS/src/Model/Model.java
+++ PSS/src/Model/Model.java
@@ -6,13 +6,12 @@ import java.util.ArrayList;
 public class Model {
 	
 	//both work the same one is direct path, the other is simple path
-	private String filepath = "C:/Users/Ben/Documents/College/Semester 2 yr 3/CS-3560/PSSWorkspace/PSSWorkspace/PSS/src/Test/";
-	private String filepath2 = "src/Test/";
+	private String filepath = "src/Test/";
 	
 	private ArrayList<Task> taskList;
 	
 	public Model(){
-		taskList = JsonReader.readTaskList(filepath2+"testTasks.json");
+		taskList = JsonReader.readTaskList(filepath+"testTasks.json");
 		System.out.print(taskList.get(0).toString());		
 	}
 }
